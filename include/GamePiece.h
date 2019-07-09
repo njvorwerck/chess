0808699821017cda4dda8cca20787f93f656a9ee
Nathan Vorwerck <njvorwerck@gmail.com>
2019-07-08 22:47:45 -0400

    initial commit

---
+++ include/GamePiece.h
@@ -0,0 +1,30 @@
+#ifndef __GAME_PIECE_H__
+#define __GAME_PIECE_H__
+
+#include <vector>
+
+#include "UtilityTypes.h"
+#include "Board.h"
+
+class GamePiece {
+
+
+public:
+
+    static const bool BLACK = false;
+    static const bool WHITE = true;
+
+    GamePiece(Position position, bool color) : mPosition(position), mColor(color) { }
+
+    virtual bool canMove(Board& board, Position position) = 0;
+
+    virtual vector<Position> possibleMoves() = 0;
+
+protected:
+
+    Position mPosition;
+    bool mColor;
+
+};
+
+#endif
