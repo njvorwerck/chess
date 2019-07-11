d501a722867a865c063e4708673c1c93eddc5900
Nathan Vorwerck <njvorwerck@gmail.com>
2019-07-10 23:03:11 -0400

    Add constructors + rename to RowCol, allow Position as typedef

--- include/UtilityTypes.h
+++ include/UtilityTypes.h
@@ -1,9 +1,13 @@
 #ifndef __UTILITY_TYPES_H__
 #define __UTILITY_TYPES_H__
 
-struct Position {
+struct RowCol {
     size_t row;
     size_t col;
+    RowCol(size_t rowIn, size_t colIn) : row(rowIn), col(colIn) { }
+    RowCol() {}
 };
+
+typedef RowCol Position;
 
 #endif
