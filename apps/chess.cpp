52fba82e365607e8a373da9e2ff018247de95eb5
Nathan Vorwerck <njvorwerck@gmail.com>
2019-07-13 20:23:32 -0400

    more testing

--- apps/chess.cpp
+++ apps/chess.cpp
@@ -7,40 +7,12 @@
 
 #include "UtilityTypes.h"
 #include "TerminalWriter.h"
+#include "TileRenderer.h"
+#include "Queen.h"
 
 
 int main (int argc, char **argv)
 {
-    /*
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-
-    printf ("lines %d\n", w.ws_row);
-    printf ("columns %d\n", w.ws_col);
-    std::string blah;
-
-    std::cout << "\033[2J";
-
-    std::string fillScreen = "";
-    for (size_t i = 0; i < w.ws_row; i++) {
-        fillScreen += "\n";
-    }
-    printf("%s", fillScreen.c_str()); 
-    std::cout << "\033[20;20H";
-
-    size_t toRemove = 0;
-    while (std::cin >> blah && blah != "a") {
-        toRemove = blah.size();
-    }
-
-    std::cout << "\033[20;" + std::to_string(20) + "H";
-    
-    for (int i = 0; i < toRemove; i++) {
-        replaceChar(20, 20 + i, 'e');
-    }
-
-    std::cout << "\033[1;1H";
-    */
 
     RowCol dims(10, 10);
     RowCol offset(10, 10);
@@ -58,6 +30,7 @@ int main (int argc, char **argv)
 
     termWriter.writeAll(window);
 
+    /*
     offset.row = 9;
     offset.col = 9;
     termWriter.writePixel(offset, TerminalPixel('z'));
@@ -66,14 +39,26 @@ int main (int argc, char **argv)
     chunk.resize(3);
     for (int i = 0; i < 3; i++) {
         chunk[i].resize(3); 
-        chunk[i][0] = TerminalPixel('0');
-        chunk[i][1] = TerminalPixel('0');
-        chunk[i][2] = TerminalPixel('0');
+        chunk[i][0] = TerminalPixel('0', red, yellow);
+        chunk[i][1] = TerminalPixel('0', red, yellow);
+        chunk[i][2] = TerminalPixel('0', red, yellow);
     }
 
     offset.row = 4;
     offset.col = 4;
     termWriter.writeChunk(offset, chunk);
+    */
+
+    Position blah(0, 0);
+    Queen q(blah, false); 
+    TileRenderer tileRenderer(3); 
+
+    offset.row = 3;
+    offset.col = 3;
+
+    TerminalPixelMatrix tile = tileRenderer.render(&q);
+
+    termWriter.writeChunk(offset, tile);
 
 
     return 0;  
